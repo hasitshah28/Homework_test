a08da42f3b584d05004d666cc43ae4487cc7fc66
hasitshah28 <hasliv2802@gmail.com>
2023-03-06 13:45:11 +0000

    Homework-Week-5

---
+++ java-practice/src/helloworld/MyName.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class MyName {
+    // [] - array, console
+    public static void main(String [] args){
+
+        System.out.print("Hasit ");
+            System.out.println("Shah");
+    }
+
+}
